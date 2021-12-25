d3322d189e6748e79178a2f22024442b3c8b44d7
Huynh Nhat Hao <haohuynh11011999@gmail.com>
2021-12-25 22:23:28 +0700

    update layout my cart

--- app/src/main/java/com/example/book_store_app/activities/MyCartActivity.java
+++ app/src/main/java/com/example/book_store_app/activities/MyCartActivity.java
@@ -5,12 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.book_store_app.R;
+import com.example.book_store_app.adapter.MyCartAdapter;
+import com.example.book_store_app.databinding.ActivityMyCartBinding;
+import com.example.book_store_app.databinding.ActivityPaymentBinding;
+import com.example.book_store_app.models.BookModel;
+import com.example.book_store_app.models.MyCartModel;
+
+import java.util.ArrayList;
 
 public class MyCartActivity extends AppCompatActivity {
+    ActivityMyCartBinding binding;
+    MyCartAdapter myCartAdapter;
+    ArrayList<MyCartModel> myCartModels;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_cart);
+        binding = ActivityMyCartBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        addControls();
+        getData();
+    }
+
+    private void getData() {
+        myCartModels.add(new MyCartModel(0, "Trầm lặng - Sức mạnh tiềm ẩn của người hướng nội", "", "author", "https://salt.tikicdn.com/cache/400x400/ts/product/ac/80/5c/f91ecd9f50a35284e3fd68accf974d1d.jpg.webp", 12.000, 1, 1, "VN-123", "IN_PROGRESS"));
+    }
+
+    private void addControls() {
+        myCartModels = new ArrayList<>();
+        myCartAdapter = new MyCartAdapter(MyCartActivity.this, myCartModels);
+        binding.lvMyCart.setAdapter(myCartAdapter);
     }
 }
--- app/src/main/java/com/example/book_store_app/adapter/MyCartAdapter.java
+++ app/src/main/java/com/example/book_store_app/adapter/MyCartAdapter.java
@@ -1,15 +1,20 @@
 package com.example.book_store_app.adapter;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.text.Html;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.example.book_store_app.R;
 import com.example.book_store_app.models.MyCartModel;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -38,13 +43,67 @@ public class MyCartAdapter extends BaseAdapter {
     }
 
 
-    public static class ViewHolder{
-        public TextView txtTitle,txtidOrder,txtStatus;
-        Button btnViewDetail,btnBuyAgain;
+    public static class ViewHolder {
+        public TextView txtTitle, txtIdOrder, txtStatus;
+        Button btnViewDetail, btnBuyAgain;
         public ImageView imgBook;
     }
+
+    private String handleColorStatus(String status) {
+        if (status == "IN_PROGRESS") {
+            return "Đang xử lý";
+        } else if (status == "CANCEL") {
+            return "Hủy bỏ";
+        }
+        return "Thành công";
+    }
+
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+    public View getView(int i, View convertView, ViewGroup viewGroup) {
+
+        final ViewHolder viewHolder;
+        if (convertView == null) {
+            viewHolder = new MyCartAdapter.ViewHolder();
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.item_my_cart_layout, null);
+            viewHolder.imgBook = convertView.findViewById(R.id.imgBookMyCart);
+            viewHolder.txtTitle = convertView.findViewById(R.id.txtTitleBookMyCart);
+            viewHolder.txtStatus = convertView.findViewById(R.id.txtStatus);
+            viewHolder.txtIdOrder = convertView.findViewById(R.id.txtIdOrder);
+
+            viewHolder.btnBuyAgain = convertView.findViewById(R.id.btnBuyAgain);
+            viewHolder.btnViewDetail = convertView.findViewById(R.id.btnViewDetail);
+
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (MyCartAdapter.ViewHolder) convertView.getTag();
+        }
+
+        MyCartModel myCartModel = (MyCartModel) getItem(i);
+        viewHolder.txtTitle.setText(myCartModel.getTitle());
+
+        Picasso.get().load(myCartModel.getImage()).placeholder(R.drawable.noimage).error(R.drawable.error).into(viewHolder.imgBook);
+        String status = myCartModel.getStatus();
+        if (status == "IN_PROGRESS") {
+            viewHolder.txtStatus.setTextColor(Color.parseColor("#fbbf24"));
+
+        } else if (status == "CANCEL") {
+            viewHolder.txtStatus.setTextColor(Color.parseColor("#ef4444"));
+        } else {
+            viewHolder.txtStatus.setTextColor(Color.parseColor("#52A593"));
+
+        }
+
+        viewHolder.txtStatus.setText(handleColorStatus(status));
+        viewHolder.txtIdOrder.setText(myCartModel.getIdOrder());
+
+//        DecimalFormat decimalFormat=new DecimalFormat("###,###,###");
+//        viewHolder.txtPrice.setText(decimalFormat.format(cartModel.getPrice()) +"đ");
+//        viewHolder.txtQuantity.setText(String.valueOf(cartModel.getQuantity()));
+//        viewHolder.rbStar.setNumStars(cartModel.getRate());
+//        viewHolder.txtAuthor.setText(cartModel.getAuthor());
+
+        return convertView;
     }
 }
